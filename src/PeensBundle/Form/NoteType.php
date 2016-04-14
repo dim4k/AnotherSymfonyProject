149b6040222b5d16a31a416e95db8e9dcba4e58e
padow <patrick.douchement@gmail.com>
2016-04-14 15:58:55 +0200

    Plop

---
+++ src/PeensBundle/Form/NoteType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PeensBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NoteType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('note')
+            ->add('session')
+            ->add('student')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PeensBundle\Entity\Note'
+        ));
+    }
+}
