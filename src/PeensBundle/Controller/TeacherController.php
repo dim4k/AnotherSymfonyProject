8bfda7b790e85deb7032aed8a7ff5d6e150f322e
padow <patrick.douchement@gmail.com>
2016-04-14 14:42:51 +0200

    CRUD

---
+++ src/PeensBundle/Controller/TeacherController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace PeensBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use PeensBundle\Entity\Teacher;
+use PeensBundle\Form\TeacherType;
+
+/**
+ * Teacher controller.
+ *
+ * @Route("/teacher")
+ */
+class TeacherController extends Controller
+{
+    /**
+     * Lists all Teacher entities.
+     *
+     * @Route("/", name="teacher_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $teachers = $em->getRepository('PeensBundle:Teacher')->findAll();
+
+        return $this->render('teacher/index.html.twig', array(
+            'teachers' => $teachers,
+        ));
+    }
+
+    /**
+     * Creates a new Teacher entity.
+     *
+     * @Route("/new", name="teacher_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $teacher = new Teacher();
+        $form = $this->createForm('PeensBundle\Form\TeacherType', $teacher);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($teacher);
+            $em->flush();
+
+            return $this->redirectToRoute('teacher_show', array('id' => $teacher->getId()));
+        }
+
+        return $this->render('teacher/new.html.twig', array(
+            'teacher' => $teacher,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Teacher entity.
+     *
+     * @Route("/{id}", name="teacher_show")
+     * @Method("GET")
+     */
+    public function showAction(Teacher $teacher)
+    {
+        $deleteForm = $this->createDeleteForm($teacher);
+
+        return $this->render('teacher/show.html.twig', array(
+            'teacher' => $teacher,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Teacher entity.
+     *
+     * @Route("/{id}/edit", name="teacher_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Teacher $teacher)
+    {
+        $deleteForm = $this->createDeleteForm($teacher);
+        $editForm = $this->createForm('PeensBundle\Form\TeacherType', $teacher);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($teacher);
+            $em->flush();
+
+            return $this->redirectToRoute('teacher_edit', array('id' => $teacher->getId()));
+        }
+
+        return $this->render('teacher/edit.html.twig', array(
+            'teacher' => $teacher,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Teacher entity.
+     *
+     * @Route("/{id}", name="teacher_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Teacher $teacher)
+    {
+        $form = $this->createDeleteForm($teacher);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($teacher);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('teacher_index');
+    }
+
+    /**
+     * Creates a form to delete a Teacher entity.
+     *
+     * @param Teacher $teacher The Teacher entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Teacher $teacher)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('teacher_delete', array('id' => $teacher->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/PeensBundle/Form/TeacherType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PeensBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeacherType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PeensBundle\Entity\Teacher'
+        ));
+    }
+}
